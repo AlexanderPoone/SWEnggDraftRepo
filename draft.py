e6b057ba14edfb0af7a203baf97dda75329bc339
Alexis Poon <alexander@link.cuhk.edu.hk>
2021-10-03 15:44:27 +0800

    Draft

--- draft.py
+++ draft.py
@@ -1,12 +1,12 @@
 ##############################################
 #
 #   Issue Management + Release Notes Generation
-#	(Automatic Bug Triage and Assignment by Topic Modelling)
+#   (Automatic Bug Triage and Assignment by Topic Modelling)
 #   Draft only
 #
-#	Author:			Alex Poon
-#	Date:		  	Sep 30, 2021
-#	Last update:  	Sep 30, 2021
+#	Author:          Alex Poon
+#	Date:           Sep 30, 2021
+#	Last update:    Sep 30, 2021
 #
 ##############################################
 
@@ -18,15 +18,24 @@ from urllib.parse import urlencode
 from urllib.request import urlopen, Request
 
 # NLP libraries
-from gensim import corpora, models, similarities, downloader			# Topic Modelling
+# from gensim import corpora, models, similarities, downloader
+from gensim.corpora.dictionary import Dictionary
+from gensim.models.ldamodel import LdaModel
+from gensim.parsing.preprocessing import preprocess_documents
+from gensim.test.utils import common_texts
+
+import matplotlib.pyplot as plt      # Don't know whether this is needed
+
 import spacy
-import matplotlib.pyplot as plt
+from spacy.lang.en.stop_words import STOP_WORDS
+
 from wordcloud import WordCloud
 
 # Standard library
 from base64 import b64encode, b64decode
 from json import dumps, loads
 from pprint import pprint
+from re import sub
 
 app = Flask(__name__)
 CORS(app)
@@ -34,10 +43,6 @@ CORS(app)
 '''
 https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
 '''
-# @app.route('/login', methods = ['GET'])
-# def login():
-# 	return redirect('https://github.com/login/oauth/authorize?client_id=34ed33a5c053d0c8e014&redirect_uri=https://dord.mynetgear.com:5351/login2&allow_signup=false')
-
 @app.route('/', methods = ['GET'])
 def index():
 	return redirect('login')
@@ -46,7 +51,6 @@ def index():
 def login():
 	'''
 	ImmutableMultiDict([('error', 'access_denied'), ('error_description', 'The user has denied your application access.'), ('error_uri', 'https://docs.github.com/apps/managing-oauth-apps/troubleshooting-authorization-request-errors/#access-denied')])
-
 	ImmutableMultiDict([('code', 'c9427c0daabef591bec8')])
 	'''
 	print(request.method)
@@ -55,7 +59,7 @@ def login():
 
 	if 'code' not in request.args:
 		query = {
-		'client_id': '34ed33a5c053d0c8e014',
+			'client_id': '34ed33a5c053d0c8e014',
 			'redirect_uri': 'https://dord.mynetgear.com:5351/login',
 			'allow_signup': False
 		}
@@ -92,9 +96,8 @@ def login():
 		response.set_cookie('access_token', cookieContent['access_token'])
 		return response
 
-@app.route('/dashboard', methods = ['GET'])
-def dashboard():
-	# Get all repos and stuff them into the template
+def getUserInfo():
+	# Get auth'd user's name and avatar
 	url = 'https://api.github.com/user'
 
 	req = Request(url)
@@ -112,7 +115,14 @@ def dashboard():
 	res = urlopen(req)
 	resJson = loads(res.read())
 
+	return resJson
+
+@app.route('/dashboard', methods = ['GET'])
+def dashboard():
+	userInfo = getUserInfo()
 	###########################
+
+	# Get all repos and stuff them into the template
 	url = 'https://api.github.com/user/repos?sort=pushed&per_page=100'
 
 	req = Request(url)
@@ -138,42 +148,46 @@ def dashboard():
 	open_issues = sum([x['open_issues'] for x in resJson2])
 	open_issue_repos = sum([1 for x in resJson2 if x != 0])
 
-	langs = [x['language'] for x in resJson2]
+	wordcloud = topicModelling()
 
 	return render_template('index.html', segment='index', 
-		avatar=resJson['avatar_url'], usrname=resJson['login'], name=resJson['name'],
-		open_issues=open_issues, open_issue_repos=open_issue_repos, repolist=resJson2)
+		avatar=userInfo['avatar_url'], usrname=userInfo['login'], name=userInfo['name'],
+		open_issues=open_issues, open_issue_repos=open_issue_repos, repolist=resJson2, wordcloud=wordcloud)
+
+@app.route('/repo/<string:owner>/<string:name>', methods = ['GET'])
+def repoDetail(owner, name):
+	userInfo = getUserInfo()
+	###########################
+
+	return render_template('index.html', segment='index', 
+		avatar=userInfo['avatar_url'], usrname=userInfo['login'], name=userInfo['name'],
+		open_issues=None, open_issue_repos=None, repolist=[])
 
 @app.route('/logout', methods = ['GET'])
 def logout():
+	#	TODO: Remove the cookie
 	return
 
 '''
-https://dord.mynetgear.com:5351/issuesToTopic
+Set up bug severity scale tags for the repository
 '''
-@app.route('/issuesToTopic', methods = ['GET'])
-def issues_to_topic(repos=['https://github.com/SoftFeta/SWEnggTestRepo']):
-	
-	# Use my account for the time being. Of course, the username and password will not be published.
-	json = {}
-	if 'token' not in json:
-		json['token'] = 'Z2hwX2JOczFHSTZPWUZmb0ZoMXpqQlBHeFhVYnVxUVB2MTRjUFVVQg=='
-		with open('_internal/credentials.txt', 'r') as f:
-			json['u'], json['p'] = f.readlines()
+@app.route('/issuesToTopic/<string:owner>/<string:name>', methods = ['GET'])
+def issues_to_topic(owner, name):
 
 	palette = [('FFCCCC', 'Trivial'),('F6B5B5', 'Trivial'),('EC9F9F', 'Minor'),('E38888', 'Minor'),('D97171', 'Moderate'),
 	('D05B5B', 'Moderate'),('C64444', 'Major'),('BD2D2D', 'Major'),('B31717', 'Critical'),('AA0000', 'Critical')]
 
-	cnt = 0
-	for lbl in palette:
-		url = f"https://api.github.com/repos/{repos[0].split('/')[-2]}/{repos[0].split('/')[-1]}/labels"
+	tok = request.cookies.get('access_token')
+	headers = {
+		'Accept': '*/*',
+		'Content-Type': 'application/json',
+		'Authorization': f"token {tok}"
+	}
 
-		auth = f"{json['u']}:{json['p']}"
-		headers = {
-			'Accept': '*/*',
-			'Content-Type': 'application/json',
-			'Authorization': f"Basic {b64encode(auth.encode('utf-8')).decode('utf-8')}"
-		}
+	cnt = 0
+
+	for lbl in palette:
+		url = f"https://api.github.com/repos/{owner}/{name}/labels"
 
 		body = {
 			'name': f'severity:{cnt+1}',
@@ -197,9 +211,72 @@ def issues_to_topic(repos=['https://github.com/SoftFeta/SWEnggTestRepo']):
 
 	return 'es klappt'
 
-#  Possibly connect to a MongoDB database for topic modelling?
-#  By topic modelling, word cloud maybe
+'''
+1. Topic model repos in Topics predefined on GitHub one by one.
+2. Classify the repo the user has chosen to one of these Topics.
+3. Add severity rating.
+
+Possibly connect to a MongoDB database
+Start button -> AJAX
+https://ieeexplore.ieee.org/document/5298419
+'''
 def topicModelling():
+	repo = 'istio'
+
+	url = f'https://api.github.com/repos/istio/{repo}/issues?per_page=100&state=all'
+
+	req = Request(url)
+
+	tok = request.cookies.get('access_token')
+
+	headers = {
+		'Accept': '*/*',
+		'Content-Type': 'application/json',
+		'Authorization': f"token {tok}"
+	}
+	for h in headers:
+		req.add_header(h, headers[h])
+
+	res = urlopen(req)
+	resJson = loads(res.read())
+	pprint(resJson)
+
+	# English only for the time being, TODO: detect language
+	nlp = spacy.load("en_core_web_trf")
+
+	from random import randint
+
+	# Remove code strings and code blocks from issue text body
+	text = [sub(r'`.*?`','',sub(r'```.*?```', '', x['body'])) for x in resJson]
+	pprint(text)
+	docs = [[chunk.text.lower() for chunk in nlp(t).noun_chunks if chunk.text.lower().replace(' ','').isalpha() and (chunk.text.lower().replace(' ','') not in [*STOP_WORDS, repo]) and len(chunk.text.lower().replace(' ','')) >= 3] for t in text]
+
+	# Generate word cloud for visualisation
+	long_string = ','.join(docs[1])
+	wordcloud = WordCloud(font_path='C:\\WINDOWS\\Fonts\\SCRIPTBL.TTF', background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
+	wordcloud.generate(long_string)
+
+
+	# Analyze syntax
+	# print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+	print(docs)
+	common_dictionary = Dictionary(docs[1:])
+	# TODO: Try TF-ID
+	common_corpus = [common_dictionary.doc2bow(t) for t in docs[1:]]
+
+	# Train the model on the corpus
+	lda = LdaModel(common_corpus, id2word=common_dictionary, num_topics=10)
+	# pprint(lda.get_topics())
+	pprint(lda.print_topics())
+
+	lda.inference([common_dictionary.doc2bow(docs[0])])
+
+	# print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+
+	# # Find named entities, phrases and concepts
+	# for entity in doc.ents:
+	#     print(entity.text, entity.label_)
+	'''
 	# Stream a training corpus directly from S3.
 	corpus = corpora.MmCorpus("s3://path/to/corpus")
 
@@ -211,7 +288,9 @@ def topicModelling():
 
 	# Compute similarity of a query vs indexed documents.
 	sims = index[query]
+	'''
 
+	return wordcloud.to_svg(embed_font=True)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5351, ssl_context=('_internal/cert.pem', '_internal/privkey.pem'))
